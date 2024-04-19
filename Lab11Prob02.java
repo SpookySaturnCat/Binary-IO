439e38defa58b7bcd6feeaa1f53b1cb7d585f515
T-ayy-lor <165335817+T-ayy-lor@users.noreply.github.com>
2024-04-19 15:56:08 -0400

    Add files via upload

--- Lab11Prob02.java
+++ Lab11Prob02.java
@@ -1,4 +1,44 @@
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/**
+ * File: Lab11Prob02.java 
+ * Class: CSCI 1302 
+ * Author: Taylor O'Neal, Seraphina Morrison 
+ * Created on: April 19th, 2024 Last Modified: April 19th,
+ * 2024 Description:
+ */
 
 public class Lab11Prob02 {
-
+	public static void main(String[] args) {
+		ArrayList<Person> people = new ArrayList<>();
+		
+		try (DataInputStream input = new DataInputStream(new FileInputStream("src/people.dat"));	
+			) {
+			while (true) {
+				people.add(new Person(input.readInt(), input.readUTF(), input.readUTF(), input.readInt(), input.readDouble()));	
+			}
+		} catch (EOFException e) {
+			System.out.println("End of file!");
+		} catch (IOException e) {
+			System.out.println("IO error!");
+		}
+		
+		Collections.sort(people);
+		
+		try (PrintWriter output = new PrintWriter("people-salary-sorted.dat");
+			) {
+			for (int i = 0; i < people.size(); i++) {
+				output.printf(people.get(i).toString());
+				System.out.println(people.get(i).toString());
+			}
+		} catch (IOException e) {
+			System.out.println("IO error");
+		}
+	}
 }
